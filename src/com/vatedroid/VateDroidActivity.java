1469c22691d5452cddc918bbfe4fcff31d1f63bc
lorinbeer <lorin@adobe.com>
2013-04-21 20:11:13 -0700

    ensufire.com vatedroid tutorial part 2 step 7 test make updated make with new v8 lib name and inclusion of local include dir
    wrote tutorial branch specific readme
    
    readme update and formatting
    
    fixed log syntax error
    
    removed tags until fixed
    
    tag documentation with descrition

--- src/com/vatedroid/VateDroidActivity.java
+++ src/com/vatedroid/VateDroidActivity.java
@@ -2,9 +2,13 @@ package com.vatedroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class VateDroidActivity extends Activity
 {
+    // native instance method declaration
+    private native String bottleThrower(String name, String code);
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
