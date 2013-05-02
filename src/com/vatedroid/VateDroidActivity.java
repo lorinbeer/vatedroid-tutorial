192b01e2536831d26afa9d17869a749847951710
lorinbeer <lorin@adobe.com>
2013-05-02 16:46:06 -0700

    vatedroid tutorial part 3 step 1 calling ndk module from java main activity

--- src/com/vatedroid/VateDroidActivity.java
+++ src/com/vatedroid/VateDroidActivity.java
@@ -6,13 +6,19 @@ import android.util.Log;
 
 public class VateDroidActivity extends Activity
 {
+    // static initializer loads native library
+    static {
+        System.loadLibrary("vatedroid");
+    }
     // native instance method declaration
-    private native String bottleThrower(String name, String code);
+    private native String feedVatedroid(String name, String code);
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        String result = feedVatedroid("VateDroid Activity", "var blank");
+        Log.d("VATEDROID ACTIVITY VATEDROID 'PRODUCED' RESULT", result);
     }
 }
